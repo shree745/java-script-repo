9de6247d1aa455ac3a57a5b0ef664e5b04e07748
Shubham Shree <103574533+shree745@users.noreply.github.com>
2024-06-20 09:07:44 +0000

    new files added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello")  // we are using node.js not browser
+
+console.log(3+3);
+
+console.log(typeof null) // so null is an object 
+console.log(typeof undefined) // undefined itself is a datatype
+
---
+++ 01_basics/03_conversiondatatype.js
@@ -0,0 +1,15 @@
+let score = 53
+console.log(typeof score);
+
+let score1 = "53"
+console.log(typeof score1)
+
+let newNumber = Number(score1)
+console.log(typeof newNumber)
+console.log(newNumber)
+
+let score2 = "53abc"
+console.log(typeof score2)
+ let newNumber2 = Number(score2)
+ console.log(typeof newNumber2);
+ console.log(newNumber2);
