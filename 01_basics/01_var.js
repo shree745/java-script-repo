2949f7aaa8da1751a23e3a21303f1350c54443c3
Shubham Shree <103574533+shree745@users.noreply.github.com>
2024-06-20 07:39:44 +0000

    Changes made

---
+++ 01_basics/01_var.js
@@ -0,0 +1,20 @@
+const accountId = 128795
+let accountEmail = "shree@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+let accountState
+ // if variable is declared and not initialized then its value is stored as undefined
+
+console.log(accountId)
+
+accountEmail = "s@gmail.com"
+accountPass = "147820"
+accountCity = "Jodhpur"
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+/* do not use var ever in js 
+because of issue in block scope and functional scope */
+
+
+
